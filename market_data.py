b6b5647c32e1455157eb00e02e82b91f0236bb09
Green Light <devyanshukhanna@gmail.com>
2026-03-09 21:56:06 +0530

    Update market_data.py

--- market_data.py
+++ market_data.py
@@ -1,16 +1,30 @@
 import requests
 
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "application/json",
+}
+
+
 def get_index(symbol):
 
     url = f"https://priceapi.moneycontrol.com/pricefeed/notapplicable/inidicesindia/{symbol}"
 
-    r = requests.get(url)
-    data = r.json()["data"]
+    r = requests.get(url, headers=headers, timeout=10)
 
-    return {
-        "price": data["pricecurrent"],
-        "change": float(data["pricepercentchange"])
-    }
+    if r.status_code != 200:
+        return {"price": "NA", "change": 0}
+
+    try:
+        data = r.json()["data"]
+
+        return {
+            "price": data["pricecurrent"],
+            "change": float(data["pricepercentchange"])
+        }
+
+    except:
+        return {"price": "NA", "change": 0}
 
 
 def get_indices():
@@ -25,7 +39,7 @@ def get_indices():
 
     result = {}
 
-    for name,code in indices.items():
+    for name, code in indices.items():
         result[name] = get_index(code)
 
     return result
